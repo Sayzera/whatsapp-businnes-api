fa849d17e29a988131c432784c7806b83a77bcf9
Sezer <sezer.boluk@niltekyazilim.com.tr>
2023-12-02 23:41:23 +0300

    first commit

---
+++ app/api/webhook/get-whatsapp-message/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface WebhookData { 
+    app: string;
+    timestamp: number;
+    version: number;
+    type: string;
+    payload: {
+        id: string;
+        source: string;
+        type:string;
+        payload: any;
+        sender: {
+            phone: string;
+            name: string;
+            country_code: string;
+            dial_code: string;
+        };
+        context: {
+            id: string;
+            gsId: string;
+        };
+    }
+}
+
+
+export async function POST(req: NextRequest) {
+    const data = req.body as WebhookData | null;
+
+    try {
+        return NextResponse.json({
+           data: data
+        },{
+            status: 200
+        })
+    }catch(error) {
+
+    }
+}
