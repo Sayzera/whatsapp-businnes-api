94e351801f0101251f0acbcc8e8898cd71d331f6
Sezer <sezer.boluk@niltekyazilim.com.tr>
2023-12-06 23:32:12 +0300

    fixed

--- app/api/webhook/get-whatsapp-message/route.ts
+++ app/api/webhook/get-whatsapp-message/route.ts
@@ -80,17 +80,13 @@ export async function POST(req: NextRequest) {
 
       if(!responseData.data.success) {
         return new NextResponse(
-          `Merhaba ${existsUser.name},
-                  ${text} numaralı başvuru bulunamadı.
-                  `,
+          `Merhaba ${existsUser.name},${text} numaralı başvuru bulunamadı.  `,
           { status: 200 }
         );
       }
 
       return new NextResponse(
-        `Merhaba ${existsUser.name},
-                ${responseData.data.data.col_trademark} markası için başvuru durumunuz '${responseData.data.data.col_last_process_status}' olarak kayıtlıdır. 
-                `,
+        `Merhaba ${existsUser.name}, ${responseData.data.data.col_trademark} markası için başvuru durumunuz '${responseData.data.data.col_last_process_status}' olarak kayıtlıdır.`,
         { status: 200 }
       );
     }
