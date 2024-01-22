f203679ecae6acf6cba3e7a1f42e69589d14faf9
Sezer <sezer.boluk@niltekyazilim.com.tr>
2024-01-22 15:08:58 +0300

    fixed

--- app/api/webhook/get-whatsapp-message/route.ts
+++ app/api/webhook/get-whatsapp-message/route.ts
@@ -41,62 +41,18 @@ export async function POST(req: NextRequest) {
         }
       );
 
-      // if (!responseData.data.success) {
-      //   return new NextResponse(
-      //     `Merhaba ${existsUser.name}, '${text}' numaralı başvuru bulunamadı.  `,
-      //     { status: 200 }
-      //   );
-      // }
+      console.log(responseData);
 
-      // return new NextResponse(
-      //   `Merhaba ${existsUser.name}, ${responseData.data.data.col_trademark} markası için başvuru durumunuz '${responseData.data.data.col_last_process_status}' olarak kayıtlıdır.`,
-      //   { status: 200 }
-      // );
+      if (!responseData.data.success) {
+        return new NextResponse(
+          `Merhaba ${existsUser.name}, '${text}' numaralı başvuru bulunamadı.  `,
+          { status: 200 }
+        );
+      }
 
-      return NextResponse.json(
-        {
-          interactive: {
-            type: "list",
-            header: {
-              type: "text",
-              text: "your-header-content",
-            },
-            body: {
-              text: "your-text-message-content",
-            },
-            footer: {
-              text: "your-footer-content",
-            },
-            action: {
-              button: "cta-button-content",
-              sections: [
-                {
-                  title: "your-section-title-content",
-                  rows: [
-                    {
-                      id: "unique-row-identifier",
-                      title: "row-title-content",
-                      description: "row-description-content",
-                    },
-                  ],
-                },
-                {
-                  title: "your-section-title-content",
-                  rows: [
-                    {
-                      id: "unique-row-identifier",
-                      title: "row-title-content",
-                      description: "row-description-content",
-                    },
-                  ],
-                },
-              ],
-            },
-          },
-        },
-        {
-          status: 200,
-        }
+      return new NextResponse(
+        `Merhaba ${existsUser.name}, ${responseData.data.data.col_trademark} markası için başvuru durumunuz '${responseData.data.data.col_last_process_status}' olarak kayıtlıdır.`,
+        { status: 200 }
       );
     }
   } catch (error) {
