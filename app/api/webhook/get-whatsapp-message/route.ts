caf46a5bb572d7bdb3a969d93fd4f0b97b7e6097
Sezer <sezer.boluk@niltekyazilim.com.tr>
2024-01-23 16:55:25 +0300

    x

--- app/api/webhook/get-whatsapp-message/route.ts
+++ app/api/webhook/get-whatsapp-message/route.ts
@@ -6,11 +6,12 @@ import {
   WebhookData,
 } from "@/types/api/webhook/get-whatsapp-message/types";
 import { userCheck } from "@/lib/api/existsUser";
+import { WhatsAppApi } from "@/lib/api/whatsApp";
 
 export async function POST(req: NextRequest) {
+  const whatsAppApi = new WhatsAppApi();
   const data: WebhookData = await req.json();
-
-  console.log("[Webhook Data]", data);
+  // console.log("[Webhook Data]", data);
 
   if (data?.payload?.type !== "text") {
     return new NextResponse("Invalid Request", { status: 400 });
@@ -28,33 +29,41 @@ export async function POST(req: NextRequest) {
           status: 200,
         }
       );
-    } else {
-      const formData = new FormData();
-      const text = data.payload?.payload?.text;
-      formData.append("col_application_number", text);
+    }
 
-      const responseData: ApizResultFileStatusData = await axios.post(
-        `${process.env.NEXT_PUBLIC_APIZ_URL}/web/hook/whatsapp/result-file-status`,
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+    const formData = new FormData();
+    const text = data.payload?.payload?.text;
+    formData.append("col_application_number", text);
 
-      if (!responseData.data.success) {
-        return new NextResponse(
-          `Merhaba ${existsUser.name}, '${text}' numaralı başvuru bulunamadı.  `,
-          { status: 200 }
-        );
+    const responseData: ApizResultFileStatusData = await axios.post(
+      `${process.env.NEXT_PUBLIC_APIZ_URL}/web/hook/whatsapp/result-file-status`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       }
+    );
 
+    if (!responseData.data.success) {
       return new NextResponse(
-        `Merhaba ${existsUser.name}, ${responseData.data.data.col_trademark} markası için başvuru durumunuz '${responseData.data.data.col_last_process_status}' olarak kayıtlıdır.`,
+        `Merhaba ${existsUser.name}, '${text}' numaralı başvuru bulunamadı.  `,
         { status: 200 }
       );
     }
+
+    let sendBasicMessage = await whatsAppApi.sendMessage({
+      message: {
+        type: "text",
+        text: `Merhaba ${existsUser.name}, ${responseData.data.data.col_trademark} markası için başvuru durumunuz '${responseData.data.data.col_last_process_status}' olarak kayıtlıdır.`,
+      },
+      destination: data.payload.sender.phone,
+    });
+
+    // return new NextResponse(
+    //   `Merhaba ${existsUser.name}, ${responseData.data.data.col_trademark} markası için başvuru durumunuz '${responseData.data.data.col_last_process_status}' olarak kayıtlıdır.`,
+    //   { status: 200 }
+    // );
   } catch (error) {
     console.log("[Webhook Error]", error);
     return new NextResponse("Invalid Request", { status: 400 });
--- lib/api/whatsApp.ts
+++ lib/api/whatsApp.ts
@@ -1,5 +1,48 @@
-class WhatsApp {
-  constructor() {
-    // private object
+interface sendMessageProps {
+  message: Record<string, unknown>;
+  destination: number | string;
+}
+
+class WhatsAppApi {
+  source: number = 905370333757;
+  channel: string = "whatsapp";
+  srcName: string = "NiltekDev";
+  apikey: string = "y0fwulucncdyfuuqgurfznibf8necwkd";
+  constructor() {}
+
+  async sendMessage({ message, destination }: sendMessageProps) {
+    const sdk = require("api")("@gupshup/v1.0#ezpvi10lcyl9hs6");
+    try {
+      sdk
+        .postMsg(
+          {
+            channel: this.channel,
+            source: this.source,
+            destination: destination,
+            message: JSON.stringify(message),
+            "src.name": this.srcName,
+            disablePreview: false,
+            encode: false,
+          },
+          {
+            apikey: this.apikey,
+          }
+        )
+        // .then(({ data }) => console.log(data))
+        .catch((err: any) => console.error(err));
+
+      return {
+        success: true,
+        message: "Message sent successfully",
+      };
+    } catch (error: any) {
+      console.log("[ERROR] WhatsAppApi.sendMessage()", error);
+      return {
+        success: false,
+        message: "Message could not be sent",
+      };
+    }
   }
 }
+
+export { WhatsAppApi };
